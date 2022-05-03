9503d16cdba7fd97704131101d8161258b42183b
gagdiez <gagdiez.c@gmail.com>
2022-05-03 12:12:56 +0200

    Simplified integration testing

--- tests/main.test.js
+++ tests/main.test.js
@@ -1,28 +1,27 @@
-describe("Hello NEAR" , function(){
+const { Near, Account, Contract } = nearAPI;
 
-  let contract
+describe("Hello NEAR", function () {
+  let contract;
 
   beforeAll(async function () {
     // NOTE: nearlib and nearConfig are made available by near-cli/test_environment
-    const near = await nearlib.connect(nearConfig)
-    const accountId = nearConfig.contractName
-    contract = await near.loadContract(nearConfig.contractName, {
-      viewMethods: ['get_greeting'],
-      changeMethods: ['set_greeting'],
-      sender: accountId
-    })
-  })
-  
-  describe("Greeter", function(){
-    it('returns the default greeting', async () => {
-      const message = await contract.get_greeting({args:{}})
-      expect(message).toBe('Hello')
-    })
-  
-    it('should change the greeting', async () => {
-      await contract.set_greeting({args:{message:'howdy'}})
-      const message = await contract.get_greeting()
-      expect(message).toBe('howdy')
-    })
-  })
-})
+    const near = await new Near(nearConfig);
+    const user = await new Account(near.connection, nearConfig.contractName);
+    contract = await new Contract(
+      user,
+      nearConfig.contractName,
+      { viewMethods: ["get_greeting"], changeMethods: ["set_greeting"] });
+  });
+
+  describe("Greeter", function () {
+    it("returns the default greeting", async () => {
+      const message = await contract.get_greeting({ args: {} });
+      expect(message).toBe("Hello");
+    });
+    it("should change the greeting", async () => {
+      await contract.set_greeting({ args: { message: "howdy" } });
+      const message = await contract.get_greeting();
+      expect(message).toBe("howdy");
+    });
+  });
+});
