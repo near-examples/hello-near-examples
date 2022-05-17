47da070738fb04463438d4cf7763412edc0d23ef
gagdiez <gagdiez.c@gmail.com>
2022-05-17 12:46:50 +0200

    Changed output contract to main.wasm

--- integration-tests/ts/main.ava.ts
+++ integration-tests/ts/main.ava.ts
@@ -14,7 +14,7 @@ test.beforeEach(async (t) => {
   const root = worker.rootAccount;
   const contract = await root.createAndDeploy(
     root.getSubAccount("hello_near").accountId,
-    "./out/hello_near.wasm",
+    "./out/main.wasm",
     { initialBalance: NEAR.parse("30 N").toJSON() }
   );
 
