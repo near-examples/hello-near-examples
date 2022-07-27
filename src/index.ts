728c5a372d843c3281787dc17e649346df6a3562
BenKurrek <bkurrek@uwaterloo.ca>
2022-07-27 14:13:44 -0400

    ts implementation

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,13 @@
 import { NearContract, NearBindgen, near, call, view } from 'near-sdk-js'
 
 // Define the default message
-const DEFAULT_MESSAGE = "Hello";
+const DEFAULT_MESSAGE: string = "Hello";
 
 // Define the contract structure
 @NearBindgen
 class Contract extends NearContract {
+    message: string;
+
     // Define the constructor, which sets the message equal to the default message.
     constructor() {
         super()
@@ -19,14 +21,14 @@ class Contract extends NearContract {
 
     @call
     // Public method - accepts a greeting, such as "howdy", and records it
-    set_greeting({ message }) {
+    set_greeting({ message }: { message: string }) {
         near.log(`Saving greeting ${message}`)
         this.message = message;
     }
     
     @view
     // Public method - returns the greeting saved, defaulting to DEFAULT_MESSAGE
-    get_greeting() {
+    get_greeting(): string {
         return this.message;
     }
 }
