1f3c75768f3311de22f019b4dfc16713b2dfa5e8
gagdiez <gagdiez.c@gmail.com>
2022-09-07 17:10:17 +0200

    updated wallet

--- frontend/near-wallet.js
+++ frontend/near-wallet.js
@@ -2,18 +2,18 @@
 import { providers } from 'near-api-js';
 
 // wallet selector UI
-import "@near-wallet-selector/modal-ui/styles.css";
-import { setupModal } from "@near-wallet-selector/modal-ui";
-import LedgerIconUrl from "@near-wallet-selector/ledger/assets/ledger-icon.png";
-import MyNearIconUrl from "@near-wallet-selector/my-near-wallet/assets/my-near-wallet-icon.png";
+import '@near-wallet-selector/modal-ui/styles.css';
+import { setupModal } from '@near-wallet-selector/modal-ui';
+import LedgerIconUrl from '@near-wallet-selector/ledger/assets/ledger-icon.png';
+import MyNearIconUrl from '@near-wallet-selector/my-near-wallet/assets/my-near-wallet-icon.png';
 
 // wallet selector options
-import { setupWalletSelector } from "@near-wallet-selector/core";
-import { setupLedger } from "@near-wallet-selector/ledger";
-import { setupMyNearWallet } from "@near-wallet-selector/my-near-wallet";
+import { setupWalletSelector } from '@near-wallet-selector/core';
+import { setupLedger } from '@near-wallet-selector/ledger';
+import { setupMyNearWallet } from '@near-wallet-selector/my-near-wallet';
 
-const THIRTY_TGAS = "30000000000000";
-const NO_DEPOSIT = "0"
+const THIRTY_TGAS = '30000000000000';
+const NO_DEPOSIT = '0';
 
 // Wallet that simplifies using the wallet selector
 export class Wallet {
@@ -22,18 +22,22 @@ export class Wallet {
   accountId;
   contractId;
 
-  constructor({contractId}){
+  constructor({contractId, createAccessKey=true}){
+    // Initializing a wallet with a contractId will add a
+    // key to the user's account.
+    // Omitting the accountId will result in the user being
+    // asked to sign all transactions.
+    this.contractWalletInit = createAccessKey ? contractId : undefined;
     this.contractId = contractId;
   }
 
   // To be called when the website loads
   async startUp() {
-    console.log("startup")
     this.walletSelector = await setupWalletSelector({
-      network: "testnet",
+      network: 'testnet',
       modules: [setupMyNearWallet({ iconUrl: MyNearIconUrl }),
-      setupLedger({ iconUrl: LedgerIconUrl })],
-    });;
+        setupLedger({ iconUrl: LedgerIconUrl })],
+    });
 
     const isSignedIn = this.walletSelector.isSignedIn();
 
@@ -44,19 +48,19 @@ export class Wallet {
       this.accountId = accounts[0].accountId;
     }
 
-    return isSignedIn
+    return isSignedIn;
   }
 
   // Sign-in method
   signIn() {
-    const description = "Please select a wallet to sign in."
-    const modal = setupModal(this.walletSelector, { contractId: this.contractId, description })
-    modal.show()
+    const description = 'Please select a wallet to sign in.';
+    const modal = setupModal(this.walletSelector, { contractId: this.contractWalletInit, description });
+    modal.show();
   }
 
   // Sign-out method
   signOut() {
-    this.wallet.signOut()
+    this.wallet.signOut();
     this.wallet = this.accountId = this.contractId = null;
     window.location.replace(window.location.origin + window.location.pathname);
   }
@@ -67,12 +71,12 @@ export class Wallet {
     const provider = new providers.JsonRpcProvider({ url: network.nodeUrl });
 
     let res = await provider.query({
-      request_type: "call_function",
+      request_type: 'call_function',
       account_id: contractId,
       method_name: method,
       args_base64: Buffer.from(JSON.stringify(args)).toString('base64'),
-      finality: "optimistic",
-    })
+      finality: 'optimistic',
+    });
     return JSON.parse(Buffer.from(res.result).toString());
   }
 
@@ -86,7 +90,7 @@ export class Wallet {
       receiverId: contractId,
       actions: [
         {
-          type: "FunctionCall",
+          type: 'FunctionCall',
           params: {
             methodName: method,
             args,
@@ -95,7 +99,7 @@ export class Wallet {
           },
         },
       ],
-    })
+    });
   }
 
   // Get transaction result from the network
@@ -104,7 +108,7 @@ export class Wallet {
     const provider = new providers.JsonRpcProvider({ url: network.nodeUrl });
 
     // Retrieve transaction result from the network
-    const transaction = await provider.txStatus(txhash, 'unnused')
-    return providers.getTransactionLastResult(transaction)
+    const transaction = await provider.txStatus(txhash, 'unnused');
+    return providers.getTransactionLastResult(transaction);
   }
 }
