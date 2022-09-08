7579a2a6f537366f440d2ed2d1977fb48a06185b
gagdiez <gagdiez.c@gmail.com>
2022-09-08 17:24:10 +0200

    formatting

--- frontend/near-interface.js
+++ frontend/near-interface.js
@@ -7,10 +7,17 @@ export class HelloNEAR {
   }
 
   async getGreeting() {
-    return await this.wallet.viewMethod({ contractId: this.contractId, method: 'get_greeting' });
+    return await this.wallet.viewMethod({
+      contractId: this.contractId,
+      method: 'get_greeting'
+    });
   }
 
   async setGreeting(greeting) {
-    return await this.wallet.callMethod({ contractId: this.contractId, method: 'set_greeting', args: { greeting: greeting } });
+    return await this.wallet.callMethod({
+      contractId: this.contractId,
+      method: 'set_greeting',
+      args: { greeting: greeting }
+    });
   }
 }
