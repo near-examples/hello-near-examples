515399be70c0fe2c4a05aad57618125ea7dbabcc
Matias Benary <matiasbenary@gmail.com>
2024-01-18 14:40:14 -0300

    feat: update and reorganize repo

---
+++ contract-ts/src/contract.ts
@@ -0,0 +1,18 @@
+import { NearBindgen, near, call, view } from 'near-sdk-js';
+
+@NearBindgen({})
+class HelloNear {
+  greeting: string = "Hello";
+
+  @view({}) // This method is read-only and can be called for free
+  get_greeting(): string {
+    return this.greeting;
+  }
+
+  @call({}) // This method changes the state, for which it cost gas
+  set_greeting({ greeting }: { greeting: string }): void {
+    // Record a log permanently to the blockchain!
+    near.log(`Saving greeting ${greeting}`);
+    this.greeting = greeting;
+  }
+}
