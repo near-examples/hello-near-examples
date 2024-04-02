e764428583772aba3f7ca82e4a7b9b13ea2919bc
gagdiez <gagdiez.c@gmail.com>
2024-04-02 15:25:32 +0200

    fix: tests

--- contract-rs/tests/test_basics.rs
+++ contract-rs/tests/test_basics.rs
@@ -1,13 +1,19 @@
+use near_sdk::NearToken;
 use serde_json::json;
+
+const FIVE_NEAR: NearToken = NearToken::from_near(5);
 
 #[tokio::test]
 async fn test_contract_is_operational() -> Result<(), Box<dyn std::error::Error>> {
     let sandbox = near_workspaces::sandbox().await?;
     let contract_wasm = near_workspaces::compile_project("./").await?;
 
-    let contract = sandbox.dev_deploy(&contract_wasm).await?;
+    let root = sandbox.root_account()?;
 
-    let user_account = sandbox.dev_create_account().await?;
+    let user_account = root.create_subaccount("user").transact().await?.unwrap();
+    let contract_account = root.create_subaccount("contract").initial_balance(FIVE_NEAR).transact().await?.unwrap();
+
+    let contract = contract_account.deploy(&contract_wasm).await?.unwrap();
 
     let outcome = user_account
         .call(contract.id(), "set_greeting")
