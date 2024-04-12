ecc9b3ab0cacd7368587f3adc1b999a401003ad6
gagdiez <gagdiez.c@gmail.com>
2024-04-12 11:39:30 +0200

    removed automatic steps

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,13 @@
+const isProduction = process.env.NODE_ENV === 'production'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    unoptimized: true,
+  },
+  basePath: isProduction? '/hello-near-examples' : '',
+  output: "export",
+  distDir: 'build',
   reactStrictMode: true,
 }
 
