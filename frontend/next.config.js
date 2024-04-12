88673a9513385d1fbc9652a230e72cdd8b8478b5
gagdiez <gagdiez.c@gmail.com>
2024-04-12 11:14:42 +0200

    no config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,12 +1,5 @@
-const isGHPages = process.env.NODE_ENV === 'GHPages'
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    unoptimized: true,
-  },
-  basePath: isGHPages && "/hello-near-examples",
-  output: "export",
   reactStrictMode: true,
 }
 
