d4aea3add3fb515fc9d680020bcc569157425e7b
Matias Benary <matiasbenary@gmail.com>
2024-04-11 21:08:53 -0300

    change nextjs

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    unoptimized: true,
+  },
   basePath: "/hello-near-examples",
   output: "export",
   reactStrictMode: true,
