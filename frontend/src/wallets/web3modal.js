a36fce89ab4d8b9d4c25e824173a0558d97c8234
Slava Karkunov <karkunow@gmail.com>
2024-09-23 15:01:15 +0100

    Fix formatting for web3wallet.js

--- frontend/src/wallets/web3modal.js
+++ frontend/src/wallets/web3modal.js
@@ -8,47 +8,47 @@ const reownProjectId = '5bb0fe33763b3bea40b8d69e4269b4ae';
 
 const onMainnet = NetworkId == "mainnet";
 const nearChain = {
-    id: EVMWalletChain.chainId,
-    name: `NEAR Protocol${ onMainnet ? "" : " Testnet"}`,
-    nativeCurrency: {
+  id: EVMWalletChain.chainId,
+  name: `NEAR Protocol${ onMainnet ? "" : " Testnet"}`,
+  nativeCurrency: {
     decimals: 18,
     name: "NEAR",
     symbol: "NEAR",
-    },
-    rpcUrls: {
+  },
+  rpcUrls: {
     default: { http: [EVMWalletChain.ethRpcForNear] },
     public: { http: [EVMWalletChain.ethRpcForNear] },
-    },
-    blockExplorers: {
+  },
+  blockExplorers: {
     default: {
-        name: "NEAR Explorer",
-        url: EVMWalletChain.walletExplorerUrl,
+      name: "NEAR Explorer",
+      url: EVMWalletChain.walletExplorerUrl,
     },
-    },
-    testnet: !onMainnet,
+  },
+  testnet: !onMainnet,
 };
 
 const url = "http://localhost:3000";
 const metadata = {
-    name: "Onboard to NEAR Protocol",
-    description: "Discover NEAR Protocol with Ethereum and NEAR wallets.",
-    url: url,
-    icons: [`${url}/near.svg`],
+  name: "Onboard to NEAR Protocol",
+  description: "Discover NEAR Protocol with Ethereum and NEAR wallets.",
+  url: url,
+  icons: [`${url}/near.svg`],
 };
 
 export const wagmiConfig = createConfig({
-    chains: [nearChain],
-    transports: {
-        [nearChain.id]: http(),
-    },
-    connectors: [
-        walletConnect({ projectId: reownProjectId, metadata, showQrModal: false }),
-        injected({ shimDisconnect: true }),
-    ],
+  chains: [nearChain],
+  transports: {
+    [nearChain.id]: http(),
+  },
+  connectors: [
+    walletConnect({ projectId: reownProjectId, metadata, showQrModal: false }),
+    injected({ shimDisconnect: true }),
+  ],
 });
 
 export const web3Modal = createWeb3Modal({
-    wagmiConfig: wagmiConfig,
-    // Get a project ID at https://cloud.walletconnect.com
-    projectId: reownProjectId,
+  wagmiConfig: wagmiConfig,
+  // Get a project ID at https://cloud.walletconnect.com
+  projectId: reownProjectId,
 });
