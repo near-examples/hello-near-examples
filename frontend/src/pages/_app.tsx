00bc443c73c579de5a85325a94df1b5afd7c473a
Matias Benary <matiasbenary@gmail.com>
2026-01-07 12:31:29 -0300

    feat: add near-connect-hook to frontend

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,12 +2,13 @@ import "@/styles/globals.css";
 
 import type { AppProps } from "next/app";
 import { Navigation } from "@/components/navigation";
+import { NearProvider } from "near-connect-hooks";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-      <>
+    <NearProvider>
       <Navigation />
       <Component {...pageProps} />
-      </>
+    </NearProvider>
   );
 }
