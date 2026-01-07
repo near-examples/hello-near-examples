957d139cfb65481b02e970a2403cdd1789a66743
Matias Benary <matiasbenary@gmail.com>
2026-01-07 13:45:08 -0300

    fix: repair build for frontend

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 
-import NearLogo from "../../public/near.svg";
-import NextLogo from "../../public/next.svg";
+import NearLogo from "/public/near.svg";
+import NextLogo from "/public/next.svg";
 import { Cards } from "@/components/cards";
 import styles from "@/styles/app.module.css";
 
